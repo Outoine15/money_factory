da8a9775434a68d2eb1c0c8ba9069e5540eff529
outoine15 <outoine15@gmail.com>
2025-09-07 11:22:28 +0200

    add store to trading list + move trading list to money.rhai (to move to trade.rhai)

--- items_data/recipe_generator.py
+++ items_data/recipe_generator.py
@@ -1,4 +1,4 @@
-	# // template
+	# // template sell point
 	# mod_info.add(
 	# 	RecipeBuilder(
 	# 		"<item>Sell:MoneyFactory",
@@ -10,23 +10,40 @@
 	# 	.with_output(WeakItemStack("Money:MoneyFactory", <itemvalue>))
 	# );
 
+	# // template store:
+	# mod_info.add(
+	# 	RecipeBuilder(
+	# 		"smolGeneralStore:MoneyFactory",
+	# 		"buy <item>", 
+	# 		0,
+	# 		"<item>Icon:VoxelEras"
+	# 	).crafted_in("smolGeneralStore:MoneyFactory")
+	# 	.with_input(WeakItemStack("Money:MoneyFactory", <itemvalue>))
+	# 	.with_output(WeakItemStack("<item>:VoxelEras", 1))
+	# );
+
 item=""
 itemvalue=0
 # print(template)
 
 import csv
 
-def template_fill(item,itemvalue):
-    template='	mod_info.add(\n		RecipeBuilder(\n			"'+item+'Sell:MoneyFactory",\n			"Sell '+item+' for Money",\n			0,\n			"MoneyIcon:MoneyFactory"\n		).crafted_in("autoSellPoint:MoneyFactory")\n        .with_input(WeakItemStack("'+item+':VoxelEras", 1))\n		.with_output(WeakItemStack("Money:MoneyFactory", '+str(itemvalue)+'))\n	);'
+def template_fill_sell_point(item,itemvalue):
+    template='	mod_info.add(\n		RecipeBuilder(\n			"'+item+'Sell:MoneyFactory",\n			"Sell '+item+' for Money",\n			0,\n			"MoneyIcon:MoneyFactory"\n		).crafted_in("autoSellPoint:MoneyFactory")\n        .with_input(WeakItemStack("'+item+':VoxelEras", 1))\n		.with_output(WeakItemStack("Money:MoneyFactory", '+str(itemvalue)+'))\n	);\n'
     return template
 
+def template_fill_store(item,itemvalue):
+	template='	mod_info.add(\n		RecipeBuilder(\n	"smolGeneralStore:MoneyFactory",\n			"buy '+item+'",\n			0,\n			"'+item+'Icon:VoxelEras"\n		).crafted_in("smolGeneralStore:MoneyFactory")\n		.with_input(WeakItemStack("Money:MoneyFactory", '+str(itemvalue)+'))\n		.with_output(WeakItemStack("'+item+':VoxelEras", 1))\n	);\n'
+	return template
 
 with open("items_data.csv","r") as csv_items_data:
-    items_data=csv.reader(csv_items_data)
-    for row in items_data:
-        item=row[0]
-        itemvalue=row[1]
-        trade_info=template_fill(item,itemvalue)
-        print(trade_info)
+	items_data=csv.reader(csv_items_data)
+	for row in items_data:
+		item=row[0]
+		itemvalue=row[1]
+		sell_point_info=template_fill_sell_point(item,itemvalue)
+		store_info=template_fill_store(item,itemvalue)
+		print(sell_point_info)
+		print(store_info)
 
 
